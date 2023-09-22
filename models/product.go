af458e8bb10fd990612f14fa550e1b670ef0e9e2
Tus1688 <76937231+Tus1688@users.noreply.github.com>
2023-09-22 22:45:12 +0700

    chore(product): add price

--- models/product.go
+++ models/product.go
@@ -7,32 +7,37 @@ import (
 )
 
 type CreateProduct struct {
-	Name        string `json:"name" binding:"required"`
-	Description string `json:"description" binding:"required"`
-	BusinessId  string `json:"business_id" binding:"required"`
+	Name        string  `json:"name" binding:"required"`
+	Description string  `json:"description" binding:"required"`
+	BusinessId  string  `json:"business_id" binding:"required"`
+	Price       float64 `json:"price" binding:"required"`
 }
 
 type ProductResponse struct {
-	Id           string `json:"id"`
-	Name         string `json:"name"`
-	Description  string `json:"description"`
-	BusinessId   string `json:"business_id"`
-	BusinessName string `json:"business_name"`
-	PhoneNumber  string `json:"phone_number"`
-	UpdatedOn    string `json:"updated_on"`
+	Id           string  `json:"id"`
+	Name         string  `json:"name"`
+	Description  string  `json:"description"`
+	BusinessId   string  `json:"business_id"`
+	BusinessName string  `json:"business_name"`
+	Price        float64 `json:"price"`
+	UpdatedOn    string  `json:"updated_on"`
 }
 
 type ModifyProduct struct {
-	Id          string `json:"id" binding:"required"`
-	Name        string `json:"name" binding:"required"`
-	Description string `json:"description" binding:"required"`
-	BusinessId  string `json:"business_id" binding:"required"`
+	Id          string  `json:"id" binding:"required"`
+	Name        string  `json:"name" binding:"required"`
+	Description string  `json:"description" binding:"required"`
+	BusinessId  string  `json:"business_id" binding:"required"`
+	Price       float64 `json:"price" binding:"required"`
 }
 
 func (c *CreateProduct) Create() error {
+	if c.Price <= 0 {
+		return fmt.Errorf("invalid input")
+	}
 	_, err := database.MysqlInstance.Exec(
-		"INSERT INTO products (name, description, business_refer) VALUES (?, ?, UUID_TO_BIN(?))", c.Name, c.Description,
-		c.BusinessId,
+		"INSERT INTO products (name, description, business_refer, price) VALUES (?, ?, UUID_TO_BIN(?), ?)", c.Name,
+		c.Description, c.BusinessId, c.Price,
 	)
 	if err != nil {
 		return err
@@ -42,7 +47,7 @@ func (c *CreateProduct) Create() error {
 
 func GetProduct(searchQuery string) ([]ProductResponse, error) {
 	query := `
-		SELECT BIN_TO_UUID(p.id), p.name, p.description, BIN_TO_UUID(p.business_refer), b.name, b.phone_number, p.updated_at
+		SELECT BIN_TO_UUID(p.id), p.name, p.description, BIN_TO_UUID(p.business_refer), b.name, price, p.updated_at
 		FROM products p
 		INNER JOIN businesses b ON b.id = p.business_refer
 	`
@@ -62,8 +67,7 @@ func GetProduct(searchQuery string) ([]ProductResponse, error) {
 	for rows.Next() {
 		var temp ProductResponse
 		err := rows.Scan(
-			&temp.Id, &temp.Name, &temp.Description, &temp.BusinessId, &temp.BusinessName, &temp.PhoneNumber,
-			&temp.UpdatedOn,
+			&temp.Id, &temp.Name, &temp.Description, &temp.BusinessId, &temp.BusinessName, &temp.Price, &temp.UpdatedOn,
 		)
 		if err != nil {
 			return nil, err
@@ -75,8 +79,8 @@ func GetProduct(searchQuery string) ([]ProductResponse, error) {
 
 func (m *ModifyProduct) Modify() error {
 	res, err := database.MysqlInstance.Exec(
-		"UPDATE products SET name = ?, description = ?, business_refer = UUID_TO_BIN(?), updated_at = NOW() WHERE id = UUID_TO_BIN(?)",
-		m.Name, m.Description, m.BusinessId, m.Id,
+		"UPDATE products SET name = ?, description = ?, business_refer = UUID_TO_BIN(?), price = ?, updated_at = NOW() WHERE id = UUID_TO_BIN(?)",
+		m.Name, m.Description, m.BusinessId, m.Price, m.Id,
 	)
 	if err != nil {
 		return err
